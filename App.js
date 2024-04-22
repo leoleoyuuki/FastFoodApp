f70bc0869e407d980c868b36a985428f10f3c747
leoleoyuuki <rm550373@fiap.com.br>
2024-04-22 13:49:29 -0300

    Update navigation and context providers in App.js

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Button, Icon, Text } from "@rneui/base";
+import { Button, Icon } from "@rneui/base";
 import ResumoCarrinho from "./src/telas/ResumoCarrinho";
-import TelaLista from "./src/telas/Home";
-import { UsuarioProvider } from "./src/context/UsuarioContext";
-import { ImageBackground } from "react-native";
+import Home from "./src/telas/Home";
+import { ComidaProvider } from "./src/context/ComidaContext";
 import { StatusBar } from "expo-status-bar";
 import Carrinho from "./src/telas/Carrinho";
 import { Ionicons } from "@expo/vector-icons";
+import Cadastro from "./src/telas/Cadastro";
+import Login from "./src/telas/Login";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <UsuarioProvider>
+    <ComidaProvider>
       <NavigationContainer>
         <Navigator
-          initialRouteName="TelaList"
+          initialRouteName="Cadastro"
           screenOptions={{
             headerShown: false,
             tabBarStyle: {
@@ -24,21 +25,40 @@ export default function App() {
               opacity: 0.7,
               padding: 0,
               margin: 0,
-
             },
           }}
         >
           <Screen
+            name="Cadastro"
+            component={Cadastro}
+            options={() => ({
+              title: "Cadastro",
+              headerShown: false,
+              tabBarButton: () => null, // Ocultar completamente o botão da tabBar
+
+            })}
+          />
+          <Screen
+            name="Login"
+            component={Login}
+            options={() => ({
+              title: "Login",
+              headerShown: false,
+              tabBarButton: () => null, // Ocultar completamente o botão da tabBar
+
+            })}
+          />
+          
+          <Screen
             name="Home"
-            component={TelaLista}
-            
+            component={Home}
             options={({ navigation }) => {
               return {
-                title:'Home',
-                headerShown:false,
-                tabBarLabel:'Home',
-                tabBarIcon:({color, size}) => (
-                    <Ionicons name="home-outline" color={color} size={size} />
+                title: "Home",
+                headerShown: false,
+                tabBarLabel: "Home",
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="home-outline" color={color} size={size} />
                 ),
                 headerRight: () => (
                   <Button
@@ -56,15 +76,17 @@ export default function App() {
             options={({ navigation }) => {
               return {
                 title: "Carrinho: ",
-                headerShown:false,
-                tabBarLabel:'Carrinho',
-                tabBarIcon:({color, size}) => (
-                    <Ionicons name="cart-outline" color={color} size={size} />
+                headerShown: false,
+                tabBarLabel: "Carrinho",
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="cart-outline" color={color} size={size} />
                 ),
                 headerRight: () => (
                   <Button
                     type="clear"
-                    icon={<Ionicons name="cart-outline" color="white" size={30} />}
+                    icon={
+                      <Ionicons name="cart-outline" color="white" size={30} />
+                    }
                     onPress={() => navigation.navigate("Carrinho")}
                   />
                 ),
@@ -77,15 +99,17 @@ export default function App() {
             options={({ navigation }) => {
               return {
                 title: "Resumo Carrinho: ",
-                headerShown:false,
-                tabBarLabel:'Resumo Carrinho',
-                tabBarIcon:({color, size}) => (
-                    <Ionicons name="cash-outline" color={color} size={size} />
+                headerShown: false,
+                tabBarLabel: "Resumo Carrinho",
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="cash-outline" color={color} size={size} />
                 ),
                 headerRight: () => (
                   <Button
                     type="clear"
-                    icon={<Ionicons name="cart-outline" color="white" size={30} />}
+                    icon={
+                      <Ionicons name="cart-outline" color="white" size={30} />
+                    }
                     onPress={() => navigation.navigate("ResumoCarrinho")}
                   />
                 ),
@@ -95,7 +119,6 @@ export default function App() {
         </Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
-    </UsuarioProvider>
-    
+    </ComidaProvider>
   );
 }
